4b47d39802638efe5bf7a38e60581ebff77fdd9d
sqz777 <sqz777isme@gmail.com>
2017-07-29 15:00:12 +0800

    add 2nd test case and failed

--- Clock in Mirror/Clock in Mirror/UnitTest1.cs
+++ Clock in Mirror/Clock in Mirror/UnitTest1.cs
@@ -15,12 +15,25 @@ namespace Clock_in_Mirror
             var expected = "12:00";
             Assert.AreEqual(expected,actual);
         }
+
+        [TestMethod]
+        public void Input_1201_Should_Be_1159()
+        {
+            var kata = new Kata();
+            var input = "12:01";
+            var actual = kata.WhatIsTheTime(input);
+            var expected = "11:59";
+            Assert.AreEqual(expected, actual);
+
+        }
+
     }
 
     public class Kata
     {
         public string WhatIsTheTime(string input)
         {
+
             return input;
         }
     }

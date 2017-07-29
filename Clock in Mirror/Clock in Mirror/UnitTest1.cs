95e35199b93173be76035fb7843319a0a6ae7726
sqz777 <sqz777isme@gmail.com>
2017-07-29 14:58:18 +0800

    add first test case and failed

--- Clock in Mirror/Clock in Mirror/UnitTest1.cs
+++ Clock in Mirror/Clock in Mirror/UnitTest1.cs
@@ -7,8 +7,21 @@ namespace Clock_in_Mirror
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Input_1200_Should_Be_1200()
         {
+            var kata = new Kata();
+            var input = "12:00";
+            var actual = kata.WhatIsTheTime(input);
+            var expected = "12:00";
+            Assert.AreEqual(expected,actual);
+        }
+    }
+
+    public class Kata
+    {
+        public string WhatIsTheTime(string input)
+        {
+            throw new NotImplementedException();
         }
     }
 }
